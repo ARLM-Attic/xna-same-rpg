9e4354ec5f5da70f7bc5db7aa1785e0212d22e78
SND\newatlanta19006_cp <SND\newatlanta19006_cp@142e9c35-602f-4880-9f37-ec48a24daa6a>
2007-05-17 00:05:18 +0000

    git-svn-id: https://xnarpg.svn.codeplex.com/svn@2519 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ XNA RPG/Input/Input.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace XNA_RPG.Input
+{
+    public enum Input
+    {
+        Confirm = Keys.F,
+        Cancel = Keys.D,
+        Menu = Keys.S,
+        Map = Keys.A,
+        Up = Keys.Up,
+        Down = Keys.Down,
+        Right = Keys.Right,
+        Left = Keys.Left
+    };
+}
